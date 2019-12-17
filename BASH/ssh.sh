9ca3885b1152af3ddf265195f1390b0095e7d2f0
dmgolembiowski <dmgolembiowski@gmail.com>
2019-12-17 18:37:08 -0500

    Oh yeah, there\'s that.

---
+++ BASH/ssh.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo # Run ssh-keygen; cat ~/.ssh/id_rsa.pub | ssh username@remote_host "mkdir -p ~/.ssh && touch ~/.ssh/authorized_keys && chmod -R go= ~/.ssh && cat >> ~/.ssh/authorized_keys"
